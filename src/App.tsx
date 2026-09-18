8b98b5269eef6f5bcd6a6b31f4cc098e472e1f9f
Subhan Osmanov <subhanosmanov625@example.com>
2026-09-18 12:13:03 +0400

    Footer componenti tamamlandı

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './footer';
 import Header from './header';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
        <Header />
+       <Footer />
     </div>
   )
 }
